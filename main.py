33de2f68f7489227e5b2eb1dc96e4940f266fd1f
Vijayabharathi Murugan <vijaybharathi1998@hotmail.com>
2025-04-09 00:57:44 -0400

    add model accuracy scores

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import os
+
 import numpy as np
 
-from src.config import DATA_CONFIG
+from src.config import DATA_CONFIG, OUTPUT_DIR
 from src.data_generator import generate_yin_yang_data
 from src.model import train_and_evaluate_models
 from src.visualization import generate_visualizations
@@ -20,6 +22,16 @@ def main():
 
     # Generate visualizations using all data points
     generate_visualizations(results, X_test, y_test)
+
+    # Save model performance score to file
+    with open(os.path.join(OUTPUT_DIR, "model_performance.txt"), "w") as f:
+        text = ""
+        for model_type, model_results in results.items():
+            text += f"{model_type}\n"
+            for model_result in model_results:
+                text += f"{model_result['model']}: {model_result['accuracy']}\n"
+            text += "\n"
+        f.write(text)
 
 
 if __name__ == "__main__":
